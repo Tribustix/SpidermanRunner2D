286d0f7e8e5079fcb4cc7c5664ff549418fd7aef
tribustix <tribustix@gmail.com>
2018-02-10 22:59:07 +0100

    Added Main Folders and Main Player Functionality
    I added some folders that I'm going to use to create this videogame and the main functionality to make that the player moves, also I added a sprite for the player.

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player{
+
+	public float  jumpForce;
+	public float speed;
+	public string secene;
+
+
+	public Player(){
+		jumpForce = 20f;
+		speed = 5f;
+	}
+
+	public Player(float jumpForce, float speed){
+		this.jumpForce = jumpForce;
+		this.speed = speed;
+	}
+
+	void playerDeath(string scene){
+		SceneManager.LoadScene(scene);
+	}
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Player player = new Player();
+
+	public Rigidbody2D myRigibody2D;
+
+
+		void Start () {
+
+			myRigibody2D = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			myRigibody2D.velocity = new Vector2(myRigibody2D.velocity.x, player.jumpForce);
+		}
+		
+		myRigibody2D.velocity = new Vector2(player.speed, myRigibody2D.velocity.y);
+		
+	}	
+}
