3db6037a7888a2f5975cb0fb9c9e27ef9e1d64b8
tribustix <tribustix@gmail.com>
2018-02-11 01:26:52 +0100

    Added Spawn Manager and Delete C# Script Scene
    I've created a Spawn Manager that I will use to create infinite platforms (and later other elements) and deleted script scene which was a mistake. Also I've added 2 platform generatrs to prefabs.

---
+++ SpiderManRunner2D/Assets/Scripts/SpawnManagerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerController : MonoBehaviour {
+
+	public GameObject[] itemPreFabs;
+
+	public float minTime = 1f;
+	public float maxTime = 2f;
+
+	 void Start() {
+		 StartCoroutine(SpawnCoroutine(0));
+	}
+
+	IEnumerator SpawnCoroutine(float waitTime){
+		yield return new WaitForSeconds(waitTime);
+		Instantiate(itemPreFabs[Random.Range(0, itemPreFabs.Length)], transform.position, Quaternion.identity );
+		StartCoroutine(SpawnCoroutine(Random.Range(minTime, maxTime)));
+	}
+}
